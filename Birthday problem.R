461c2ef358576db0be9da87484ec043c00100f2b
khan98es <shahbaz98es@yahoo.com>
2021-03-19 17:41:12 +0000

    Committing my first file with Git.

---
+++ Birthday problem.R
@@ -0,0 +1,8 @@
+B <- 10^seq(1,5,len=100)
+compute_prob <- function(B,n=22){
+  same_day <- replicate(B,{
+    bdays <- sample(1:365,n,replace=TRUE)
+    any(duplicated(bdays))
+  })
+  mean(same_day)
+}
